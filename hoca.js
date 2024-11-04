01160814afef0953c911a4708eb605e5a91f1ce6
dhv2006 <5001103088@student.hcmue.edu.vn>
2024-11-04 08:44:49 +0700

    First commit

---
+++ hoca.js
@@ -0,0 +1,12 @@
+function myFunction1() {
+    document.body.style.backgroundImage = "url('./wallpaperflare.com_wallpaper.jpg')";
+    document.body.style.rowGap = 'no-repeat';
+    document.body.style.attachment = 'fixed';
+    document.body.style.size = '100% 100%';
+  }
+function myFunction2() {
+    document.body.style.backgroundImage = "url('./pngtree-underwater-shipwreck-fish-school-background-picture-image_2443766.jpg')";
+    document.body.style.rowGap = 'no-repeat';
+    document.body.style.attachment = 'fixed';
+    document.body.style.size = '100% 100%';
+  }
